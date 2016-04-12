aea1985f6a5adb2968ccc90f79f18cfae8482ca5
dmathew93 <denzel.mathew@gmail.com>
2016-04-12 16:20:36 -0400

    jorge take this

--- src/controllers/MainMenuViewController.java
+++ src/controllers/MainMenuViewController.java
@@ -1,11 +1,14 @@
 package controllers;
 
+import utilities.GameStateManager;
+import views.View;
+
 /**
  * Created by denzel on 4/11/16.
  */
 public class MainMenuViewController extends Controller {
 
-    //
+    //constructor of the mainMenuController
     public MainMenuViewController(){
 
     }
