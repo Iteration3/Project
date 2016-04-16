9bc51813967f52cd599ca528a105316e42efb1be
Andres Borjas <aborjas@ufl.edu>
2016-04-15 21:54:14 -0400

    small changes

--- src/models/AreaEffect/FlowTile.java
+++ src/models/AreaEffect/FlowTile.java
@@ -1,6 +1,7 @@
 package models.AreaEffect;
 
 import models.Entity.Entity;
+import utilities.Location.Location;
 
 /**
  * Created by Andy on 4/14/2016.
@@ -8,5 +9,16 @@ import models.Entity.Entity;
 public class FlowTile extends AreaEffect {
 
 
-    public void execute(Entity e){}
+    public void execute(Entity e){
+        flow(e);
+    }
+
+    private void flow(Entity e){
+        //yeah I should probably figure this out.
+        int x,y;
+        Location temp = loc;
+        y =temp.getCol();
+        
+        e.location();
+    }
 }
--- src/models/AreaEffect/Trap.java
+++ src/models/AreaEffect/Trap.java
@@ -2,10 +2,23 @@ package models.AreaEffect;
 
 import models.Entity.Entity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Andy on 4/14/2016.
  */
 public class Trap extends AreaEffect {
 
-    public void execute(Entity e){}
+    public void execute(Entity e){
+        trap(e);
+    }
+
+    private void trap(Entity e){
+        Map statChange = new HashMap<>();
+        statChange.put("Movement",-100);
+        e.modifyStats(statChange);
+
+        //okay so how the fuck do i do this. Need to wait a certain amount of time and then do something.
+    }
 }
