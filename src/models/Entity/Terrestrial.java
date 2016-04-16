7d1bf1e5ca530415263c88b874f21eb9a6370383
chausen <clayhausen@gmail.com>
2016-04-15 22:14:58 -0400

    Still working on Movement.

---
+++ src/models/Entity/Terrestrial.java
@@ -0,0 +1,15 @@
+package models.Entity;
+
+import models.Map.Terrain;
+
+/**
+ * Created by clayhausen on 4/15/16.
+ */
+public class Terrestrial extends Locomotion {
+
+    @Override
+    public void move(Terrain terrain) {
+
+    }
+
+}
