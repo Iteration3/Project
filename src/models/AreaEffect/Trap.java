f7dd6cdd53fc75b922c262512b4b43bcb4500dec
Andres Borjas <aborjas@ufl.edu>
2016-04-16 19:28:57 -0400

    trap should therotically work

--- src/models/AreaEffect/Trap.java
+++ src/models/AreaEffect/Trap.java
@@ -5,24 +5,58 @@ import utilities.Location.Location;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Andy on 4/14/2016.
  */
 public class Trap extends AreaEffect {
 
+    private boolean active;
+    private Timer timer = new Timer();
     public Trap(Location l){
         loc = l;
+        active = true;
     }
     public void execute(Entity e){
         trap(e);
     }
 
     private void trap(Entity e){
-        Map statChange = new HashMap<>();
-        statChange.put("Movement",-100);
-        e.modifyStats(statChange);
+        if(active) {
+            //double previous = e.statValue("Movement");
 
-        //okay so how the fuck do i do this. Need to wait a certain amount of time and then do something.
+            Map statChange = new HashMap<>();
+            Map statChange2 = new HashMap<>();
+            statChange.put("Movement", -100);
+            statChange2.put("Movement",100);
+            e.modifyStats(statChange);
+
+            forceToWait();              //this should cause it to wait 5 seconds before executing this
+            e.modifyStats(statChange2);
+        }
+
+        //disable();
+    }
+
+    public synchronized void forceToWait() {
+        this.timer.cancel(); //this will cancel the current task. if there is no active task, nothing happens
+        this.timer = new Timer();
+
+        TimerTask action = new TimerTask() {
+            public void run() {
+                disable(); //as you said in the comments: abc is a static method
+            }
+
+        };
+
+        this.timer.schedule(action, 5000); //this starts the task
+    }
+
+
+
+    private void disable(){
+        active = false;
     }
 }
