ddc50c38b3fd32e580c7d8409b910f4a25e19874
chausen <clayhausen@gmail.com>
2016-04-15 20:45:48 -0400

    Working on Movement...removed the MovementKeyCommand hierarchy and will now be using an aggregate of Entity - Locomotion - to check for Terrain-based movement logic.

--- src/Utilities/KeyCommand/MovementKeyCommand.java
+++ src/Utilities/KeyCommand/MovementKeyCommand.java
@@ -1,13 +1,17 @@
 package Utilities.KeyCommand;
 import models.Entity.Entity;
 import models.Map.Map;
-import Utilities.Direction.Direction;
+import models.Direction.Direction;
+import models.Map.Tile;
 
 /**
  * Created by clayhausen on 4/13/16.
  */
 
-public abstract class MovementKeyCommand implements KeyCommand {
+/** Responsibilities: checks for obstacles preventing Entity unit,
+ *  then tells its Entity to move, passing it the Terrain it's moving to.
+ */
+public class MovementKeyCommand implements KeyCommand {
 
     private Map map;
     private Entity entity;
@@ -15,33 +19,60 @@ public abstract class MovementKeyCommand implements KeyCommand {
 
 
     @Override
-    public abstract void execute();
+    public void execute() {
+
+        entity.changeDirection(direction);
+
+        boolean canMove = true;
+        /* Tile tile = map.getTile( direction );
+
+        if ( checkForEntities() || checkForObstacles() ) {
+            canMove = false;
+        }
+        */
+
+        if ( canMove ) {
+            // Terrain terrain = tile.getTerrain();
+            // entity.move(terrain);
+        }
+
+    }
 
 
     //TODO modify once Map/Obstacle have been implemented
     // Returns true if an Entity is occupying the Tile to be moved to
-    protected boolean checkForEntities() {
+    private boolean checkForEntities(Tile tile) {
         /*
         boolean occupied = true;
-        Tile tile = map.getTile( direction );
-        if ( map.getEntity == null ) { occupied = false; }
+
+        if ( tile.getEntity == null ) { occupied = false; }
             return occupied;
          */
             return true;
-        }
+    }
 
 
-        //TODO modify once Map/Obstacle have been implemented
-        // Returns true if an Obstacle is occupying the Tile to be moved to
-        protected boolean checkForObstacles() {
+    //TODO modify once Map/Obstacle have been implemented
+    // Returns true if an Obstacle is occupying the Tile to be moved to
+    private boolean checkForObstacles(Tile tile) {
         /*
         boolean occupied = true;
-            Tile tile = map.getTile( direction );
-            if ( map.getObstacle == null ) { occupied = false; }
+
+            if ( tile.getObstacle == null ) { occupied = false; }
             return occupied;
 
         */
             return true;
-        }
+    }
+
+    //TODO update once functionality has been added in Entity and elsewhere
+    private boolean checkTerrain() {
+        return true;
+    }
+
+    // Accessor methods
+    protected Map getMap() { return map; }
+    protected Entity getEntity() { return entity; }
+    protected Direction getDirection() { return direction; }
 
 }
