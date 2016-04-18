43834a50f58acaf4c79c1b857e2f2b0561d54125
dmathew93 <denzel.mathew@gmail.com>
2016-04-17 20:41:31 -0400

    ai ??? profit ??

--- src/controllers/PetController.java
+++ src/controllers/PetController.java
@@ -1,12 +1,26 @@
-package controllers;
-
-/**
- * Created by denzel on 4/16/16.
- */
-public class PetController {
-
-    public PetController(){
-
-    }
-
-}
+//package controllers;
+//
+//import AI.AIController;
+//import AI.FSM;
+//import AI.FSMState;
+//import models.Entity.Pet;
+//
+///**
+// * Created by denzel on 4/16/16.
+// */
+//public class PetController extends AIController{
+//
+//    //pet states?
+//    FSMState[] states = {
+//            new FSMState(FSMState.States.WANDER)
+//    };
+//
+//    //The FSM then takes in the states
+//    FSM fsm = new FSM(states);
+//
+//    //pass in the FSM and the entity to the controller
+//    public PetController(Pet pet, FSM fsm){
+//        super(pet,fsm);
+//    }
+//
+//}
