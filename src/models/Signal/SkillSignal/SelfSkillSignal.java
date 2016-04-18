882ae8f3957fee3481d32eb850a53da75e62fcb5
mtschiggfrie <mtschiggfrie@gmail.com>
2016-04-18 00:44:49 -0400

    all but 5 skills work

--- src/models/Signal/SkillSignal/SelfSkillSignal.java
+++ src/models/Signal/SkillSignal/SelfSkillSignal.java
@@ -1,8 +1,14 @@
 package models.Signal.SkillSignal;
 
+import models.Decal.CircleDash;
+import models.Decal.Decal;
 import models.Entity.Entity;
 import models.Map.Map;
 import models.Skill.Skill;
+import utilities.Location.Location;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class SelfSkillSignal extends SkillSignal {
@@ -14,7 +20,22 @@ public class SelfSkillSignal extends SkillSignal {
 
     protected void useSkill(Skill skill) {
         if (skill.canUseSkill(avatar)) {
-            skill.activate(avatar);
+
+            Decal decal = new CircleDash();
+
+            Timer t = new Timer();
+            t.schedule(new TimerTask() {
+                public void run() {
+                    map.getTileAt(avatarLocation).addDecal(decal);
+                    skill.activate(avatar);
+                    if (currentRadius == 1) {
+                        map.getTileAt(avatarLocation).removeDecal();
+                        t.cancel();
+                    }
+                    currentRadius++;
+                }
+            }, 0, 1000);
+
         }
     }
 
